b206a33f8eb2e97e4fdaf80e8b01d3825fc54703
grzegorz <grzegorz.szczepaniak18@gmail.com>
2018-10-15 16:29:00 +0200

    working project configuration for docker

---
+++ UserService/src/main/java/com/scheduler/userservice/Service/Impl/UserServiceImpl.java
@@ -0,0 +1,6 @@
+package com.scheduler.userservice.Service.Impl;
+
+import com.scheduler.userservice.Service.UserService;
+
+public class UserServiceImpl implements UserService {
+}
---
+++ UserService/src/main/java/com/scheduler/userservice/Service/UserService.java
@@ -0,0 +1,7 @@
+package com.scheduler.userservice.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserService {
+}
