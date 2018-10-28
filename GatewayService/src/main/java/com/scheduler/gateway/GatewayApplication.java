84ce55a0f7059f019cb186015d618f5bb48c4c15
grzegorz <grzegorz.szczepaniak18@gmail.com>
2018-10-28 12:02:28 +0100

    using zuul instead of spring gateway, repairing tests and configuration for security

--- GatewayService/src/main/java/com/scheduler/gateway/GatewayApplication.java
+++ GatewayService/src/main/java/com/scheduler/gateway/GatewayApplication.java
@@ -3,9 +3,13 @@ package com.scheduler.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableZuulProxy
+//to
+//@EnableOAuth2Sso
 public class GatewayApplication {
 
     public static void main(String[] args) {
---
+++ GatewayService/src/main/java/com/scheduler/gateway/SecurityConfig.java
@@ -0,0 +1,25 @@
+//package com.scheduler.gateway;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.annotation.Order;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//@EnableWebSecurity
+//@Order(-10)
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests()
+//                .anyRequest().authenticated();
+//    }
+//
+//    @Override
+//    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication().withUser("root").password("password").roles("USER");
+//    }
+//}
