aa9071ccf817b12989a70276eca970a4b62d009c
grzegorz <grzegorz.szczepaniak18@gmail.com>
2018-11-17 23:09:01 +0100

    refactor

--- remove_images.sh
+++ remove_images.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-docker image rm -f services_authorization-service:latest services_book-service:latest services_config-service:latest services_discovery-service:latest services_gateway-service:latest
+docker image rm -f \
+ services_authorization-service:latest \
+ services_book-service:latest \
+ services_config-service:latest \
+ services_discovery-service:latest \
+ services_gateway-service:latest
