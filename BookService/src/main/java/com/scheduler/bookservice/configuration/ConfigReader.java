85de0e2ee3166c13bb4317162eb674e9036b9046
grzegorz <grzegorz.szczepaniak18@gmail.com>
2018-12-22 14:57:29 +0100

    moving configuration to config service

--- BookService/src/main/java/com/scheduler/bookservice/configuration/ConfigReader.java
+++ BookService/src/main/java/com/scheduler/bookservice/configuration/ConfigReader.java
@@ -14,4 +14,15 @@ public class ConfigReader {
     private String applicationKey;
     private String applicationCx;
     private Integer connectionTimeout;
+    private ConfirmationUri confirmationUri;
+
+    @Data
+    public static class ConfirmationUri {
+        private String host;
+        private String port;
+        private String protocol;
+        private String key;
+        private String endpoint;
+    }
+
 }
--- BookService/src/main/java/com/scheduler/bookservice/service/cover/implementations/ConfirmationUriCoverBuilder.java
+++ BookService/src/main/java/com/scheduler/bookservice/service/cover/implementations/ConfirmationUriCoverBuilder.java
@@ -1,16 +1,29 @@
 package com.scheduler.bookservice.service.cover.implementations;
 
+import com.scheduler.bookservice.configuration.ConfigReader;
 import com.scheduler.bookservice.service.cover.BookCover;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ConfirmationUriCoverBuilder {
 
-    public static final String HOST = "localhost";
-    public static final String PORT = "8060";
-    public static final String PROTOCOL = "http";
-    public static final String ENDPOINT = "book";
-    public static final String KEY = "confirmationCover";
+    private final ConfigReader configReader;
+
+    private final String HOST;
+    private final String PORT;
+    private final String PROTOCOL;
+    private final String ENDPOINT;
+    private final String KEY;
+
+    public ConfirmationUriCoverBuilder(ConfigReader configReader) {
+        this.configReader = configReader;
+        this.KEY = configReader.getConfirmationUri().getKey();
+        this.ENDPOINT = configReader.getConfirmationUri().getEndpoint();
+        this.PROTOCOL = configReader.getConfirmationUri().getProtocol();
+        this.PORT = configReader.getConfirmationUri().getPort();
+        this.HOST = configReader.getConfirmationUri().getHost();
+
+    }
 
     public String buildUri(BookCover bookCover) {
         return PROTOCOL + "://" + HOST + ":" + PORT + "/" + ENDPOINT + "/" + KEY + "/" + bookCover.getBook().getId();
